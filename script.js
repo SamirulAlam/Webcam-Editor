680f56d7ff73430a8029f6eb1d91f6a4f17f1985
Samirul Alam <samirulalam372@gmail.com>
2021-08-01 07:20:09 +0600

    project completed

--- script.js
+++ script.js
@@ -17,18 +17,10 @@ var blueFilter = 0
 var grayscaleFilter = 0
 
 
-
-
 let pixels = null
 let originalPixels = null
 let currentPixels = null
 
-red.onchange = runPipeline
-green.onchange = runPipeline
-blue.onchange = runPipeline
-brightness.onchange = runPipeline
-grayscale.onchange = runPipeline
-contrast.onchange = runPipeline
 
 const getVideo = () => {
     navigator.mediaDevices.getUserMedia({
@@ -54,12 +46,6 @@ const paintToCanvas = () => {
         pixels = ctx.getImageData(0, 0, width, height)
 
 
-        //getVideo()
-        // pixels=redEffect(pixels)
-        // pixels=rgbSplit(pixels)
-        // ctx.globalAlpha =0.1
-        // pixels=greenScreen(pixels)
-
         copydat = ctx.getImageData(0, 0, width, height)
         originalPixels = pixels.data.slice()
         runPipeline()
@@ -74,94 +60,118 @@ const takePhoto = () => {
     const data = canvas.toDataURL("/image/jpeg")
     const link = document.createElement("a")
     link.href = data;
-    link.setAttribute("download", "handsome")
+    link.setAttribute("download", "webfie")
     link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
     gallery.insertBefore(link, gallery.lastChild)
 }
 
-const redEffect = (pixels) => {
-    for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i + 0] = pixels.data[i + 0] + 100
-        pixels.data[i + 1] = pixels.data[i + 1] - 50
-        pixels.data[i + 2] = pixels.data[i + 2] * 0.5
-    }
-    return pixels
-}
-
-const rgbSplit = (pixels) => {
-    for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i - 150] = pixels.data[i + 0]
-        pixels.data[i + 500] = pixels.data[i + 1]
-        pixels.data[i - 550] = pixels.data[i + 2]
-    }
-    return pixels
-}
-
-const greenScreen = (pixels) => {
-    let levels = {}
-    document.querySelectorAll(".rgb input").forEach((input) => {
-        levels[input.name] = input.value
-    })
-
-    for (let i = 0; i < pixels.data.length; i += 4) {
-        const red = pixels.data[i + 0]
-        const green = pixels.data[i + 1]
-        const blue = pixels.data[i + 2]
-        const alpha = pixels.data[i + 3]
-
-        if (red >= levels.rmin
-            && green >= levels.gmin
-            && blue >= levels.bmin
-            && red <= levels.rmax
-            && blue <= levels.bmax
-            && green <= levels.gmax) {
-            pixels.data[i + 3] = 0
-        }
-    }
-    return pixels
-}
 
 const R_OFFSET = 0
 const G_OFFSET = 1
 const B_OFFSET = 2
 
-function getIndex(x, y) {
+const getIndex=(x, y)=> {
     return (x + y * video.videoWidth) * 4
 }
 
-function clamp(value) {
+const clamp=(value)=> {
     return Math.max(0, Math.min(Math.floor(value), 255))
 }
 
-function addRed(x, y, value) {
+const addRed=(x, y, value)=> {
     const index = getIndex(x, y) + R_OFFSET
     const currentValue = currentPixels[index]
     currentPixels[index] = clamp(currentValue + value)
 }
 
-function addGreen(x, y, value) {
+const addGreen=(x, y, value)=> {
     const index = getIndex(x, y) + G_OFFSET
     const currentValue = currentPixels[index]
     currentPixels[index] = clamp(currentValue + value)
 }
 
-function addBlue(x, y, value) {
+const addBlue=(x, y, value)=> {
     const index = getIndex(x, y) + B_OFFSET
     const currentValue = currentPixels[index]
     currentPixels[index] = clamp(currentValue + value)
 }
 
-function addBrightness(x, y, value) {
+const addBrightness=(x, y, value)=> {
     addRed(x, y, value)
     addGreen(x, y, value)
     addBlue(x, y, value)
 }
 
-getVideo()
+const setGrayscale=(x, y)=> {
+    const redIndex = getIndex(x, y) + R_OFFSET
+    const greenIndex = getIndex(x, y) + G_OFFSET
+    const blueIndex = getIndex(x, y) + B_OFFSET
 
-video.addEventListener("canplay", paintToCanvas)
+    const redValue = currentPixels[redIndex]
+    const greenValue = currentPixels[greenIndex]
+    const blueValue = currentPixels[blueIndex]
 
-function runPipeline() {
+    const mean = (redValue + greenValue + blueValue) / 3
+
+    currentPixels[redIndex] = clamp(mean)
+    currentPixels[greenIndex] = clamp(mean)
+    currentPixels[blueIndex] = clamp(mean)
+}
+
+const addContrast=(x, y, value)=> {
+    const redIndex = getIndex(x, y) + R_OFFSET
+    const greenIndex = getIndex(x, y) + G_OFFSET
+    const blueIndex = getIndex(x, y) + B_OFFSET
+
+    const redValue = currentPixels[redIndex]
+    const greenValue = currentPixels[greenIndex]
+    const blueValue = currentPixels[blueIndex]
+    const alpha = (value + 255) / 255
+
+    const nextRed = alpha * (redValue - 128) + 128
+    const nextGreen = alpha * (greenValue - 128) + 128
+    const nextBlue = alpha * (blueValue - 128) + 128
+
+    currentPixels[redIndex] = clamp(nextRed)
+    currentPixels[greenIndex] = clamp(nextGreen)
+    currentPixels[blueIndex] = clamp(nextBlue)
+}
+
+var kernel = 5
+
+const blurReg=(x, y, kernel)=> {
+    var tot = 0
+    var tot1 = 0
+    var tot2 = 0
+    for (let n = x - (Math.floor(kernel / 2)); n <= x + Math.floor(kernel / 2); n++) {
+        for (let k = y - (Math.floor(kernel / 2)); k <= y + Math.floor(kernel / 2); k++) {
+            tot += (pixels.data[getIndex(n, k)] || pixels.data[getIndex(x, y)])
+            tot1 += (pixels.data[getIndex(n, k) + 1] || pixels.data[getIndex(x, y) + 1])
+            tot2 += (pixels.data[getIndex(n, k) + 2] || pixels.data[getIndex(x, y) + 2])
+        }
+    }
+    tot = Math.floor(tot / (kernel * kernel));
+    tot1 = Math.floor(tot1 / (kernel * kernel));
+    tot2 = Math.floor(tot2 / (kernel * kernel));
+    pixels.data[getIndex(x, y)] = tot
+    pixels.data[getIndex(x, y) + 1] = tot1
+    pixels.data[getIndex(x, y) + 2] = tot2
+}
+
+const blurx=()=> {
+    kernel = getel('blr').value * 1
+    kernel += (1 & !(kernel & 1))
+    for (let n = 0; n < pixels.width; n++) {
+        for (let k = 0; k < pixels.height; k++) {
+            blurReg(n, k, kernel)
+        }
+    }
+    ctx.putImageData(pixels, 0, 0, 0, 0, video.videoWidth, video.videoHeight)
+
+}
+
+
+const runPipeline=()=> {
 
     currentPixels = originalPixels.slice()
 
@@ -194,45 +204,42 @@ function runPipeline() {
     commitChanges()
 }
 
-function commitChanges() {
+const commitChanges=()=> {
 
     for (let i = 0; i < pixels.data.length; i++) {
         pixels.data[i] = currentPixels[i]
     }
-    //ctx.putImageData(pixels, 0, 0, 0, 0, video.videoWidth, video.videoHeight)
 }
 
-function setGrayscale(x, y) {
-    const redIndex = getIndex(x, y) + R_OFFSET
-    const greenIndex = getIndex(x, y) + G_OFFSET
-    const blueIndex = getIndex(x, y) + B_OFFSET
+red.onchange = runPipeline
+green.onchange = runPipeline
+blue.onchange = runPipeline
+brightness.onchange = runPipeline
+grayscale.onchange = runPipeline
+contrast.onchange = runPipeline
 
-    const redValue = currentPixels[redIndex]
-    const greenValue = currentPixels[greenIndex]
-    const blueValue = currentPixels[blueIndex]
-
-    const mean = (redValue + greenValue + blueValue) / 3
-
-    currentPixels[redIndex] = clamp(mean)
-    currentPixels[greenIndex] = clamp(mean)
-    currentPixels[blueIndex] = clamp(mean)
+const getel=(x)=> {
+    return document.getElementById(x)
 }
 
-function addContrast(x, y, value) {
-    const redIndex = getIndex(x, y) + R_OFFSET
-    const greenIndex = getIndex(x, y) + G_OFFSET
-    const blueIndex = getIndex(x, y) + B_OFFSET
+const reset=()=> {
+    for (let n = 0; n < copydat.data.length; n++) {
+        pixels.data[n] = copydat.data[n];
+    }
 
-    const redValue = currentPixels[redIndex]
-    const greenValue = currentPixels[greenIndex]
-    const blueValue = currentPixels[blueIndex]
-    const alpha = (value + 255) / 255
+    getel('red').value = "0"
+    getel('green').value = "0"
+    getel('blue').value = "0"
+    getel('brightness').value = "0"
+    getel('contrast').value = "0"
+    getel('blr').value = "0"
+    grayscale.checked=false
 
-    const nextRed = alpha * (redValue - 128) + 128
-    const nextGreen = alpha * (greenValue - 128) + 128
-    const nextBlue = alpha * (blueValue - 128) + 128
 
-    currentPixels[redIndex] = clamp(nextRed)
-    currentPixels[greenIndex] = clamp(nextGreen)
-    currentPixels[blueIndex] = clamp(nextBlue)
+    ctx.putImageData(pixels, 0, 0, 0, 0, video.videoWidth, video.videoHeight)
+
 }
+
+getVideo()
+
+video.addEventListener("canplay", paintToCanvas)
